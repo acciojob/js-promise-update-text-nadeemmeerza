0aa96dd4ca22d1cd9883bf5f52762200e3c9b67a
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-07-26 23:16:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
-let output = document.getElementById("output");
+
 const call = new Promise((res, rej)=>{
 	 setTimeout(() => {
     res("Hello, world!");
   }, 1000);
 })
 call().then((res)=>{
+	let output = document.getElementById("output");
 	output.innerText = res;
 });
